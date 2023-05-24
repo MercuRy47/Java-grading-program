37b9463ab1c69dd68c44ed1743a75f182f3dbc08
MERCURY <122726524+MercuRy47@users.noreply.github.com>
2023-05-24 11:10:03 +0700

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,32 @@
+// โปรแกรมประมวลผลการเรียน(ตัดเกรด)
+
+package Chapter3;
+
+import java.util.Scanner;
+
+public class test3 {
+    public static void main(String[] args) {
+        int score;
+        char grade = ' ';
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Input your score: ");
+        score = kb.nextInt();
+
+        if(score >= 90) {
+            grade = 'A';
+        }
+        else if(score >= 80 && score <=89 ) {
+            grade = 'B';
+        }
+        else if(score >= 70 && score <=79 ) {
+            grade = 'C';
+        }
+        else if(score >= 60 && score <=69 ) {
+            grade = 'D';
+        }
+        else if(score < 60) {
+            grade = 'F';
+        } 
+        System.out.println("Grade: " + grade);
+    }
+}
